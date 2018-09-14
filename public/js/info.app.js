943926ab99b8060eccde0ab5d8cc25d21ac4ab72
rmwillli <42611731+rmwillli@users.noreply.github.com>
2018-09-13 22:06:13 -0400

    Updating Info with Button

--- public/js/info.app.js
+++ public/js/info.app.js
@@ -1,7 +1,88 @@
 var personalInfo = new Vue({
   el: '#personalInfo',
   data: {
-    name: 'Max Williams',
-    message: 'Hello Vue!'
+    result:
+    {
+      gender: "",
+      "name": {
+        "title": "",
+        "first": "",
+        "last": ""
+      },
+      "location": {
+        "street": "",
+        "city": "",
+        "state": "",
+        "postcode": "",
+        "coordinates": {
+          "latitude": "",
+          "longitude": ""
+        },
+        "timezone": {
+          "offset": "",
+          "description": ""
+        }
+      },
+      "email": "",
+      "login": {
+        "uuid": "",
+        "username": "",
+        "password": "",
+        "salt": "",
+        "md5": "",
+        "sha1": "",
+        "sha256": ""
+      },
+      "dob": {
+        "date": "",
+        "age": 0
+      },
+      "registered": {
+        "date": "",
+        "age": 0
+      },
+      "phone": "",
+      "cell": "",
+      "id": {
+        "name": "",
+        "value": ""
+      },
+      "picture": {
+        "large": "",
+        "medium": "",
+        "thumbnail": ""
+      },
+      "nat": "NO"
+    }
+  ,
+  "info": {
+    "seed": "2da87e9305069f1d",
+    "results": 1,
+    "page": 1,
+    "version": "1.2"
   }
+
+  },
+  computed: {
+
+  },
+  methods: {
+    pretty_date: function (d) {
+      return moment(d).format('l')
+    },
+
+    fetchProject () {
+  fetch('https://randomuser.me/api/')
+  .then( response => response.json() )
+  .then( json => {personalInfo.result = json.results[0]} )
+  .catch( err => {
+    console.log('PROJECT FETCH ERROR:');
+    console.log(err);
+  })
+}
+    //userapp.somethingelse = json.results[0]
+  },
+  created () {
+      this.fetchProject();
+    }
 })
