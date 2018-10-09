a1be3c493fa46fc5f5c388b8e0e449ad87b082c6
rmwillli <42611731+rmwillli@users.noreply.github.com>
2018-10-09 10:56:12 -0400

    testing php

--- app/common.php
+++ app/common.php
@@ -2,6 +2,7 @@
 // Change the working directory to this file.
 chdir(__DIR__);
 set_include_path (__DIR__);
+
 require 'environment.php';
 
 /** MODELS **/
--- public/api/comment.php
+++ public/api/comment.php
@@ -1,15 +1,16 @@
 <?php
 require '../../app/common.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'workPost.php';
   exit;
 }
 $id = intval($_GET['id'] ?? 0);
 if ($id < 1) {
-  throw new Exception('Invalid Task ID');
+  throw new Exception('Invalid ID');
 }
 // 1. Go to the database and get all work associated with the $taskId
-$workArr = Comment::getCommentById($id);
+$commentArr = Comment::getCommentById($id);
 // 2. Convert to JSON
 $json = json_encode($commentArr, JSON_PRETTY_PRINT);
 // 3. Print
