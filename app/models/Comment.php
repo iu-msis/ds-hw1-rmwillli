9e136041aa408c8293f7c0ec350da4b2d1f6a9ca
rmwillli <42611731+rmwillli@users.noreply.github.com>
2018-10-10 11:31:52 -0400

    revised php

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -10,24 +10,24 @@ class Comment
     $this->comment = $row['comment'];
   }
 
+    public function createComment(){
+      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+      $sql = 'INSERT INTO Comment (comment) VALUES (?)';
+      $statement = $db->prepare($sql);
+      $success = $statement->execute([$this->comment]);
+      $this->id = $db->lastInsertId();
+    }
+
   public static function getAllComments() {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     $sql = 'SELECT * FROM Comment';
     $statement = $db->prepare($sql);
     $success = $statement->execute();
-    $commentArr = [];
+    $arr = [];
     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
       $comment =  new Comment($row);
-      array_push($CommentArr, $comment);
+      array_push($arr, $comment);
     }
-    return $commentArr;
-  }
-
-  public function createComment(){
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    $sql = 'INSERT INTO Comment (comment) VALUES (?)';
-    $statement = $db->prepare($sql);
-    $success = $statement->execute([$this->comment]);
-    $this->id = $db->lastInsertId();
+    return $arr;
   }
 }
