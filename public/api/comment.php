41ed76f77573e6b7e06c94aa6e5afa6240467c2f
rmwillli <42611731+rmwillli@users.noreply.github.com>
2018-10-09 20:31:40 -0400

    Test if merging is working

--- public/api/comment.php
+++ public/api/comment.php
@@ -5,14 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'workPost.php';
   exit;
 }
-<<<<<<< HEAD
 
-=======
-$id = intval($_GET['id'] ?? 0);
-if ($id < 1) {
-  throw new Exception('Invalid ID');
-}
->>>>>>> parent of 768a1b7... PHP Updates
 // 1. Go to the database and get all work associated with the $taskId
 $commentArr = Comment::getAllComments();
 // 2. Convert to JSON
