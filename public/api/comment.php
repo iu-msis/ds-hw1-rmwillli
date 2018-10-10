cd552257c5311b17a6965df6cd1f39848a62716a
rmwillli <42611731+rmwillli@users.noreply.github.com>
2018-10-09 20:18:41 -0400

    revert back to before everything fell apart

--- public/api/comment.php
+++ public/api/comment.php
@@ -2,12 +2,17 @@
 require '../../app/common.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $comment = new Comment($_POST);
-  $comment->create();
-  echo json_encode($comment);
+  require 'workPost.php';
   exit;
 }
+<<<<<<< HEAD
 
+=======
+$id = intval($_GET['id'] ?? 0);
+if ($id < 1) {
+  throw new Exception('Invalid ID');
+}
+>>>>>>> parent of 768a1b7... PHP Updates
 // 1. Go to the database and get all work associated with the $taskId
 $commentArr = Comment::getAllComments();
 // 2. Convert to JSON
